66253efc04187e29def4e2977e8ce321655b32f7
gabbyo6 <gabbyo6@icloud.com>
2022-05-02 17:53:14 -0700

    General nested message structure

---
+++ client/src/components/Post/Post.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import { Thread } from "../Thread/Thread";
+import "./Post.css";
+
+export const Post = ({ message }) => {
+  console.log(message);
+  return (
+    <>
+      {message.length > 0 ? (
+        <Thread messages={message} />
+      ) : (
+        <>
+          <div className="post">
+            <h3>{message.name}</h3>
+            <p>{message.post}</p>
+          </div>
+        </>
+      )}
+    </>
+  );
+};
